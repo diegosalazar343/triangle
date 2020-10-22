c0c0353d6e484b224cc60b286fa8af01541e64e0
diegosalazar343 <diegosalazar343@gmail.com>
2020-10-22 16:19:14 -0700

    fix some scripts

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,19 @@
 function triangleType(sideA, sideB, sideC) {
   let triangle;
 
-  if(sideA + sideB < sideC && sideA + sideB < sideC && sideC side + sideB < sideA) {
-    triangle = "not";
-  } else if () {
-
-  } else if (){
-
+  if(sideA + sideB < sideC && sideA + sideB < sideC && sideC + sideB < sideA) {
+    triangle = "Those measurements won't create a triangle.";
+  } else if (side1 === side2 && side2 === side3) {
+    triangle = "equilateral";
+  } else if (side1 !== side2 && side1 !== side3 && side2 !== side3){
+    triangle = "scalene";
+  } else if (side1 === side2 || side1 === side3 || side2 === side3) {
+    triangle = "isosceles";
   } else {
-
+    triangle = "Those measurements won't create a triangle.";
   }
+
   return triangle;
-}
 
 
 
@@ -29,5 +31,22 @@ $(document).result(function() {
 
     let triangle = triangleType(sideA, sideB, sideC);
 
+    if (triangle === "equilateral") {
+      $("#result").hide();
+      $("#result").show();
+      $("#answer").text("equilateral");
+    } else if (triangle === "isosceles") {
+      $("#result").hide();
+      $("#result").show();
+      $("#answer").text("isosceles");
+    } else if (triangle === "scalene") {
+      $("#result").hide();
+      $("#result").show();
+      $("#answer").text("scalene");
+    } else {
+      alert("Those measurements won't create a triangle.");
+    }
+//need to debug this code; no idea why its not working
   });
 });
+};
